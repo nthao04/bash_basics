d573a18db90143160bbcc66855149623a2bc3ce3
Nikki <nikkithao127@gmail.com>
2024-03-31 17:18:15 -0400

    Added script to ask for file name and make empty file

--- 01-read_input.sh
+++ 01-read_input.sh
@@ -10,3 +10,8 @@ echo 'Your name was stored in $name'
 
 # exercise: write a script that asks the user for a 
 # filename and create an empty file named after it
+
+echo "Enter a file name"
+read filename
+touch $filename
+echo "File created"
